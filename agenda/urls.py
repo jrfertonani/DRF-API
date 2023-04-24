ed82b94671cd2071f4a522a59ffa8a18363d1bbf
Ademir <ademirjuniorfertonani@gmail.com>
2023-04-24 12:09:57 -0300

    Adcionando views para lista e exibir agendamento

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from agenda.views import agendamento_detail
+from agenda.views import agendamento_detail, agendamento_list
 
 urlpattherns = [
-   #path('agendamentos/',agendamento_list)
+    path('agendamentos/', agendamento_list),
     path('agendamentos/<int:id>',agendamento_detail)
 ]
+
--- agenda/views.py
+++ agenda/views.py
@@ -1,11 +1,22 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view 
 
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
 # Create your views here.
+@api_view(http_method_names=["GET"])
 def agendamento_detail(request, id):
     obj = get_object_or_404(Agendamento, id=id)
     serializer = AgendamentoSerializer(obj)
     return JsonResponse(serializer.deta)
+
+@api_view(http_method_names=["GET"])
+def agendamento_list(request):
+    qs = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(qs, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+
+ 
