c338b84e70eece9465a30b7a6126dd78f33ed1da
Ademir <ademirjuniorfertonani@gmail.com>
2023-04-29 23:03:28 -0300

    Refatorando o codigo

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from django.utils import timezone
+
+from agenda.models import Agendamento
 
 
 class AgendamentoSerializer(serializers.Serializer):
@@ -7,3 +10,32 @@ class AgendamentoSerializer(serializers.Serializer):
     email_cliente = serializers.EmailField()
     telefone_cliente = serializers.CharField(max_length=20)
 
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Agendamento não pode ser feto no passado!")
+        return value
+
+    def validate(self, attrs):
+        telefone_cliente = attrs.get("telefone_cliente", "")
+        email_cliente = attrs.get("email_cliente", "")
+
+        if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_client.startswith("+55"):
+            raise serializers.ValidationError("E-mail brasileinro deve estar associado a um numero do Brasil (+55)")
+        return attrs
+
+    def create(self, validated_data):
+        agendamento = Agendamento.object.create(
+                data_horario=validated_data["data_horario"],
+                nome_cliente=validated_data["nome_cliente"],
+                email_cliente=validated_data["email_cliente"],
+                telefone_cliente=validated_data["telefone_cliente"]
+            )
+        return agendamento
+
+    def update(self, instance, validated_data):
+         instance.data_horario = validated_data.get("data_horario", instance.data_horario)
+         instance.nome_vcliente = validated_data.get("nome_vcliente", instance.nome_cliente)
+         instance.email_cliente = validated_data.get("email_cliente", instance.email_cliente)
+         instance.telefone_cliente = validated_data.get("telefone_cliente", instance.telefone_client)
+         instance.save()
+         return instance
--- agenda/views.py
+++ agenda/views.py
@@ -1,18 +1,28 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view 
+from rest_framework.response import Response 
 
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
-# Create your views here.
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
     obj = get_object_or_404(Agendamento, id=id)
-    serializer = AgendamentoSerializer(obj)
-    return JsonResponse(serializer.deta)
+    if request.method == "GET":
+        serializer = AgendamentoSerializer(obj)
+        return JsonResponse(serializer.deta)
+    if request.request.method == "PUT":
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400) 
+    if request.method == "DELETE":
+        obj.delete()
+        return Response(status=204)
 
-@api_view(http_method_names=["GET", "POST"])
+@api_view(http_method_names=["GET", "PATCH"])
 def agendamento_list(request):
     if request.method == "GET":
         qs = Agendamento.objects.all()
@@ -22,12 +32,11 @@ def agendamento_list(request):
         data = request.data
         serializer = AgendamentoSerializer(data=data)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            Agendamento.object.create(
-                data_horario=validated_data["data_horario"],
-                nome_cliente=validated_data["nome_cliente"],
-                email_cliente=validated_data["email_cliente"],
-                telefone_cliente=validated_data["telefone_cliente"]
-            )
+            serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.erros, status=400)
+
+
+
+
+
