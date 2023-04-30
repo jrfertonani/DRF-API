3ca8114aaab42b0712296c51664971c50031feea
Ademir <ademirjuniorfertonani@gmail.com>
2023-04-29 21:08:17 -0300

    Adicionando Agendamento POST

--- agenda/views.py
+++ agenda/views.py
@@ -12,11 +12,22 @@ def agendamento_detail(request, id):
     serializer = AgendamentoSerializer(obj)
     return JsonResponse(serializer.deta)
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
-    qs = Agendamento.objects.all()
-    serializer = AgendamentoSerializer(qs, many=True)
-    return JsonResponse(serializer.data, safe=False)
-
-
- 
+    if request.method == "GET":
+        qs = Agendamento.objects.all()
+        serializer = AgendamentoSerializer(qs, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == "POST":
+        data = request.data
+        serializer = AgendamentoSerializer(data=data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            Agendamento.object.create(
+                data_horario=validated_data["data_horario"],
+                nome_cliente=validated_data["nome_cliente"],
+                email_cliente=validated_data["email_cliente"],
+                telefone_cliente=validated_data["telefone_cliente"]
+            )
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.erros, status=400)
